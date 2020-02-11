733a77aff66a02fcc34ce68211cb9db51434eb38
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2020-02-11 22:01:57 +0100

    First not sounding like complete shit method of generating chords

--- src/helpers/sound.js
+++ src/helpers/sound.js
@@ -1,37 +1,50 @@
 import Tone from "tone"
 
 export default function sound(aliveCells, board, speed) {
-  const cols = board[0].length
-  const numberOfAliveCells = aliveCells.length
+  const lowestOctave = 3
+  const octaveRange = 4
+  const numberOfRows = board.length
   const time = (speed / 1000) * 0.7
-  // let chord = ["C4", "E4", "F#4", "B4", "E5"]
-  // let chord = ["E4", "A4"]
+  // let notes = ["C", "D", "E", "F", "G", "A", "B"]
+  // let notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+  // let notes = ["E", "G#", "B", "G", "A#", "C#", "D#"]
+  // let notes = ["C", "D", "E", "F#", "A", "A#"]
+  // let notes = ["C", "C#", "D#", "F", "G", "A", "A#"]
+  // let notes = ["D", "E", "F#", "G#", "B"]
+  let notes = ["C", "F", "A#", "D#", "G#"]
+  // let notes = ["C", "C#", "D#", "E", "F#", "G", "A", "A#"]
+  // let notes = ["C", "E", "F#", "G", "B"]
   let chord = []
-
-  for (let i = 0; i < numberOfAliveCells; i++) {
-    const freq = aliveCells[i][0] * 100 + ((aliveCells[i][1] + 1) / cols) * 100
-    chord.push(freq)
+  let aliveCellsPerRow = []
+  let numberOfNotes = notes.length
+  aliveCells.forEach(value => {
+    if (typeof aliveCellsPerRow[value[0]] === "undefined") {
+      aliveCellsPerRow[value[0]] = 0
+    }
+    aliveCellsPerRow[value[0]] += value[1]
+  })
+  console.log(aliveCellsPerRow)
+  for (let i = 0; i < numberOfRows; i++) {
+    if (typeof aliveCellsPerRow[i] !== "undefined") {
+      const octave = Math.floor((i / numberOfRows) * octaveRange) + lowestOctave
+      const tone = notes[aliveCellsPerRow[i] % numberOfNotes]
+      chord.push(`${tone}${octave}`)
+    }
   }
+  const chordNoDuplicates = [...new Set(chord)]
 
-  const synth = new Tone.PolySynth(numberOfAliveCells, Tone.Synth, {
-    volume: -40,
+  const synth = new Tone.PolySynth(chordNoDuplicates.length, Tone.Synth, {
+    volume: -20,
     oscillator: {
       type: "sine",
     },
     envelope: {
-      attack: time / 3,
+      attack: time / 2,
       decay: time / 10,
       sustain: 0.9,
-      release: time / 2,
+      release: time,
     },
   }).toMaster()
-  // console.log(chord)
 
-  // const synth = new Tone.Synth().toMaster()
-  synth.triggerAttackRelease(chord, `${time}`)
-  // if (interval !== 0) {
-  //   synth.triggerAttackRelease(chord, `${interval / 1000}`)
-  // } else {
-  //   synth.triggerAttackRelease(chord, `${interval / 1000}`)
-  // }
+  synth.triggerAttackRelease(chordNoDuplicates, `${time}`)
 }
