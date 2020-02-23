6ca3772f19f1e3a2866612fb603746e0a8c9f204
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2020-02-23 23:31:57 +0100

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SEO from "../components/seo";
+import Layout from "../components/layout.js";
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <p>Dolerm ipsum</p>
+  </Layout>
+);
+
+export default IndexPage;
