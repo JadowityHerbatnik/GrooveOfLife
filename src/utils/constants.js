3abc0f92ab82d7d2f6763e87c18c3317ebd86abb
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2020-03-22 00:14:14 +0100

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const sizes = {
+  preferredCellSize: 30,
+  headerHeight: "15vh",
+};
+export const music = {
+  highestOctave: 6,
+  octaveRange: 4,
+};
+export const keyboard = {
+  blackWidth: "24px",
+  whiteWidth: "30px",
+  blackHeight: "85px",
+  whiteHeight: "130px",
+  margin: "1px",
+};
+export const colors = {};
+export const chords = {};
---
+++ src/utils/paragraphs.js
@@ -0,0 +1,21 @@
+import React from "react";
+import StyledAnchor from "../components/generic/StyledAnchor.js";
+
+const WikiLink = (
+  <StyledAnchor
+    target="_blank"
+    href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
+  >
+    Wikipedia
+  </StyledAnchor>
+);
+export const intro = (
+  <>
+    <span style={{ fontFamily: "Geo" }}>The Game of life </span>
+    is a cool cellular automata invented by John Conway. If you wanna read a nice
+    definition, here's a {WikiLink} link, I'm not gonna copypaste it.
+    <br />
+    <br />
+    Essentially, each cell can be alive (active) or dead (inactive)
+  </>
+);
