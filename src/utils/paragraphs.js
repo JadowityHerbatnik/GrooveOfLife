1a536840f31221b740aa1361fbd616ee2cc9d60e
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2020-03-24 22:40:37 +0100

    Some new text

--- src/utils/paragraphs.js
+++ src/utils/paragraphs.js
@@ -9,13 +9,28 @@ const WikiLink = (
     Wikipedia
   </StyledAnchor>
 );
+const TonejsLink = (
+  <StyledAnchor target="_blank" href="https://tonejs.github.io/">
+    Tone.js
+  </StyledAnchor>
+);
 export const intro = (
   <>
-    <span style={{ fontFamily: "Geo" }}>The Game of life </span>
-    is a cool cellular automata invented by John Conway. If you wanna read a nice
-    definition, here's a {WikiLink} link, I'm not gonna copypaste it.
+    {/* <span style={{ fontFamily: "Geo" }}> The Sound of life </span> */}
+    Yeah, this is yet another implementation of the famous Game of Life...but this one
+    utilizes {TonejsLink} library!
     <br />
-    <br />
-    Essentially, each cell can be alive (active) or dead (inactive)
+    <br /> Game of Life is a cool cellular automata invented by John Conway. If you wanna
+    read a nice definition, here's a {WikiLink} link, I'm not gonna copypaste it.
   </>
 );
+export const rules = (
+  <>
+    Each cell can be "alive" (white) or dead depending on how many alive neighbours it
+    has. If an active cell has more than 3 neighbours or less than 2 it'll die of
+    overpopulation or solitude respectively.
+    <br /> If a dead has exactly 3 neighbours, the neighbours will reproduce and the cell
+    will become alive (so lifelike!)
+  </>
+);
+export const keybindings = <>You can use some keyboard shortcuts to control the Game</>;
