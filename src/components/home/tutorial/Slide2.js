d8fcf49185fbcd10d63c9d893e81d1c71abb5eee
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2020-04-30 14:04:33 +0200

    Unified font sizes across carousel slides

--- src/components/home/tutorial/Slide2.js
+++ src/components/home/tutorial/Slide2.js
@@ -5,7 +5,7 @@ import { FlexBox, ScalableText } from "@common/Generic";
 export const Slide2 = () => {
   return (
     <div className="d-flex h-100 align-items-center justify-content-center">
-      <ScalableText min={13} max={19} as="div">
+      <ScalableText min={13} max={20} as="div">
         Use the slider to adjust game speed
         <FlexBox>
           <SpeedSlider />
